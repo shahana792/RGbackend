9b78d0a0de8cf33498c9fc16df9c895b695fedc2
shahana792 <qureshishahana792@gmail.com>
2024-08-09 14:08:25 +0530

    Backend Code

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv =require('dotenv')
+const cors =require('cors')
+const houseRouter = require('./routes/houseRoutes');
+const enquiryRouter = require('./routes/enquiryRoutes');
+const userRouter = require('./routes/userRoutes');
+const app = express();
+dotenv.config()
+const port = process.env.PORT;
+
+
+app.use(express.json())
+app.use(cors())
+
+
+const db = module.exports = async()=> {
+    try{
+        await mongoose.connect(process.env.MONGODBURL,{
+            user : process.env.DBUSER,
+            pass : process.env.DBPASS
+        })
+        console.log("MongoDB Connection is successful")
+    }catch(e){
+        console.log(e);
+        console.log("MongoDB Connection is not successful")
+    }
+}
+db();
+app.use("/house/",houseRouter)
+app.use("/enquiry/",enquiryRouter)
+app.use("/user/",userRouter)
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
